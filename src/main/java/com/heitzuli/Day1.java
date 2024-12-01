73caee878b1d88185e61b1af4cfd16aa14274f74
heitzuli <heidi.poels@gmail.com>
2024-12-01 13:02:27 +0200

    Refactoring: day 1, riddle 1

--- src/main/java/com/heitzuli/Day1.java
+++ src/main/java/com/heitzuli/Day1.java
@@ -12,44 +12,53 @@ import java.util.stream.Stream;
 public class Day1 {
     public static void main(String[] args) {
         try {
-            new Day1().getLocations();
+            System.out.println(new Day1().getTotalDistance());
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
     }
 
-    private void getLocations() throws URISyntaxException {
+    private Integer getTotalDistance() throws URISyntaxException {
         var fileName = "Day1.txt";
         Path path = Paths.get(Objects.requireNonNull(getClass().getClassLoader().getResource(fileName)).toURI());
         try (Stream<String> linestream = Files.lines(path)) {
-            var lines = linestream.toList();
-
-            // Split the lines into two separate lists
-            var firsts = new ArrayList<Integer>();
-            var seconds = new ArrayList<Integer>();
-
-            for (String line : lines) {
-                var x = line.split(" {3}");
-                firsts.add(Integer.parseInt(x[0]));
-                seconds.add(Integer.parseInt(x[1]));
-            }
+            Pair pair = getPair(linestream);
 
             // Sort the lists in numerical order
-            firsts.sort(Integer::compareTo);
-            seconds.sort(Integer::compareTo);
+            pair.firsts().sort(Integer::compareTo);
+            pair.seconds().sort(Integer::compareTo);
 
             // Calculate the distance between each pair
             var totalDistance = 0;
-            for (int i = 0; i < firsts.size(); i++) {
-                var first = firsts.get(i);
-                var second = seconds.get(i);
+            for (int i = 0; i < pair.firsts().size(); i++) {
+                var first = pair.firsts().get(i);
+                var second = pair.seconds().get(i);
                 var distance = Math.abs(first - second);
                 totalDistance += distance;
             }
 
-            System.out.println(totalDistance);
+            return totalDistance;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static Pair getPair(Stream<String> linestream) {
+        var lines = linestream.toList();
+
+        // Split the lines into two separate lists
+        var firsts = new ArrayList<Integer>();
+        var seconds = new ArrayList<Integer>();
+
+        for (String line : lines) {
+            var x = line.split(" {3}");
+            firsts.add(Integer.parseInt(x[0]));
+            seconds.add(Integer.parseInt(x[1]));
+        }
+        Pair result = new Pair(firsts, seconds);
+        return result;
+    }
+
+    private record Pair(ArrayList<Integer> firsts, ArrayList<Integer> seconds) {
     }
 }
