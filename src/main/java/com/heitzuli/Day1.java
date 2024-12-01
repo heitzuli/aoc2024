b00fbd863bde3b97c8e83f372e3f14464f06d5a0
heitzuli <heidi.poels@gmail.com>
2024-12-01 12:55:24 +0200

    Solution for day 1, riddle 1

--- src/main/java/com/heitzuli/Day1.java
+++ src/main/java/com/heitzuli/Day1.java
@@ -5,6 +5,7 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.stream.Stream;
 
@@ -20,8 +21,33 @@ public class Day1 {
     private void getLocations() throws URISyntaxException {
         var fileName = "Day1.txt";
         Path path = Paths.get(Objects.requireNonNull(getClass().getClassLoader().getResource(fileName)).toURI());
-        try (Stream<String> lines = Files.lines(path)) {
-            lines.forEach(System.out::println);
+        try (Stream<String> linestream = Files.lines(path)) {
+            var lines = linestream.toList();
+
+            // Split the lines into two separate lists
+            var firsts = new ArrayList<Integer>();
+            var seconds = new ArrayList<Integer>();
+
+            for (String line : lines) {
+                var x = line.split(" {3}");
+                firsts.add(Integer.parseInt(x[0]));
+                seconds.add(Integer.parseInt(x[1]));
+            }
+
+            // Sort the lists in numerical order
+            firsts.sort(Integer::compareTo);
+            seconds.sort(Integer::compareTo);
+
+            // Calculate the distance between each pair
+            var totalDistance = 0;
+            for (int i = 0; i < firsts.size(); i++) {
+                var first = firsts.get(i);
+                var second = seconds.get(i);
+                var distance = Math.abs(first - second);
+                totalDistance += distance;
+            }
+
+            System.out.println(totalDistance);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
